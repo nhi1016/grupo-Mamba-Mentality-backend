535b2cee2f086bc3d29e49634f8f86f11151cf34
Gerardo CF <gicastro@uc.cl>
2023-05-25 16:23:56 -0400

    chore(/Modelos):created new tables

--- src/models/createTables.js
+++ src/models/createTables.js
@@ -10,49 +10,83 @@ const client = new Client({
   database: process.env.DB_NAME,
 });
 
-const tabla1 = `CREATE TABLE Usuario(
+const tablas = [`CREATE TABLE Usuario(
                     id 			SERIAL PRIMARY KEY, 
 					nikname 	VARCHAR(30) UNIQUE NOT NULL,
 					password 	VARCHAR(90)
-				)`;
-const tabla2 = `CREATE TABLE Partida(
+				)`,
+				`CREATE TABLE Partida(
 					id 				SERIAL PRIMARY KEY,
 					score 			INT DEFAUL 0,
 					vidas			INT DEFAULT 3,
 					tiempo_restante	INT DEFAULT 60,
-				)`;
-const tabla3 = `CREATE TABLE Historial(
+				)`,
+				`CREATE TABLE Historial( 
 					id SERIAL 	PRIMARY KEY,
 					id_usuario 	INT,
 					id_partida 	INT,
 					fecha DATETIME,
 					FOREIGN KEY(id_usuario) REFERENCES Usuario(id),
 					FOREIGN KEY(id_partida) REFERENCES Partida(id)
-				)`;
-const tabla4 = 'CREATE TABLE Ruta(id SERIAL PRIMARY KEY,nombre VARCHAR(30) UNIQUE NOT NULL)';
-const tabla5 = 'CREATE TABLE Casilla(id SERIAL PRIMARY KEY,nombre VARCHAR(30) UNIQUE NOT NULL)';
-const tabla6 = 'CREATE TABLE RutaCasillas(id SERIAL PRIMARY KEY,nombre_ruta VARCHAR(30) ,nombre_casilla VARCHAR(30) ,posicion INT)';
-const tabla7 = 'CREATE TABLE Equipo(id SERIAL PRIMARY KEY,vida INT DEFAULT 100,suerte INT DEFAULT 10,nombre VARCHAR(30),etica INT DEFAULT 10,id_usuario INT,id_tablero INT,turno INT,id_ruta INT,id_casilla INT,FOREIGN KEY(id_usuario) REFERENCES Usuario(id),FOREIGN KEY(id_tablero) REFERENCES Tablero(id) ON DELETE CASCADE,FOREIGN KEY(id_ruta) REFERENCES Ruta(id),FOREIGN KEY(id_casilla) REFERENCES Casilla(id))';
-const tabla8 = 'CREATE TABLE EquipoPersonaje(id SERIAL PRIMARY KEY,id_equipo INT,id_personaje INT,FOREIGN KEY(id_equipo) REFERENCES Equipo(id),FOREIGN KEY(id_personaje) REFERENCES Personaje(id))';
-
-const tabla10 = 'CREATE TABLE Session(id SERIAL PRIMARY KEY,user_id INT REFERENCES "Users"(id))';
+				)`,
+				`CREATE TABLE Tablero(
+					id 			SERIAL PRIMARY KEY,
+					tamano 		INT NOT NULL,
+					dificultad 	VARCHAR(30)
+				)`,
+				`CREATE TABLE Tablero_Partida(
+					id 			SERIAL PRIMARY KEY,
+					id_partida 	INT,
+					id_tablero	INT,
+					FOREIGN KEY(id_partida) REFERENCES Partida(id),
+					FOREIGN KEY(id_tablero) REFERENCES Tablero(id)
+				)`,
+				`CREATE TABLE Imagen(
+					id 			SERIAL PRIMARY KEY,
+					nombre 		VARCHAR(30) UNIQUE NOT NULL,
+					dificultad 	VARCHAR(30)
+				)`,
+				`CREATE TABLE Tablero_Imagenes(
+					id 			SERIAL PRIMARY KEY,
+					id_imagen	INT NOT NULL,
+					FOREIGN KEY(id_imagen) REFERENCES Imagen(id)
+				)`,
+				`CREATE TABLE R_Im_Im(
+					id 			SERIAL PRIMARY KEY,
+					id_img1 	INT NOT NULL,
+					id_img2		INT NOT NULL,
+					FOREIGN KEY(id_img1) REFERENCES Imagen(id),
+					FOREIGN KEY(id_img2) REFERENCES Imagen(id)
+				)`,
+				`CREATE TABLE Bonus(
+					id 		SERIAL PRIMARY KEY,
+					tipo 	VARCHAR(30),
+					descripción VARCHAR(60)
+				)`,
+				`CREATE TABLE Partida_Bonus(
+					id 			SERIAL PRIMARY KEY,
+					id_partida	INT NOT NULL,
+					id_bonus	INT NOT NULL,
+					FOREIGN KEY(id_partida) REFERENCES Partida(id),
+					FOREIGN KEY(id_bonus) REFERENCES Bonus(id)
+				)`]
 
 const crearTablas = async () => {
-  try {
-    await client.connect();
-    await client.query(tabla6);
-    console.log('Tablas RutaCasillas creadas exitosamente !!');
-    await client.query(tabla10);
-    console.log('Tablas Session creadas exitosamente !!');
+	tablas.forEach((instruction) => {
+		const nombreTabla = instruction.split(' ')[2].slice(0,-1);
+		try {
+			await client.connect();
+			await client.query(instruction);
+			console.log(`Tabla: ${nombreTabla} creada exitosamente !!`);
 
-    await client.end();
-    console.log('Tablas creadas exitosamente !!');
-  } catch (err) {
-    console.log(err.stack);
-    console.log('Error al crear las tablas !!');
-    await client.end();
-  }
-};
+			await client.end();
+		} catch (err) {
+			console.log(err.stack);
+			console.log(`¡¡ Error al crear Tabla: ${nombreTabla[]} !!`);
+
+			await client.end();
+		}
+	})}
 
 // Abilitar esto para que funcione
 // crearTablas();
